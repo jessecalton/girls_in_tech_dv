06e1eac5c18ad2a75baf1940e97517d90e4e5c44
Jesse Calton <jessecalton@gmail.com>
2017-07-08 19:05:32 -0700

    links css style sheet and adds functionality to buttons

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
 $(function(){ 
   $('button#need-help').on('click',function(){
+    var r = $('<p>This works</p>')
+    $('.types-of-help').append(r);
+    });
+});
+$(function(){ 
+  $('button#know-someone').on('click',function(){
+    $('#shelter-housing').toggle();
+    });
+}); 
+$(function(){ 
+  $('button#dont-know').on('click',function(){
     var r = $('<p>This works</p>')
     $('.types-of-help').append(r);
     });
